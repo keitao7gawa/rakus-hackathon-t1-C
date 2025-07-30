68f3017c4eaa9bc40d23a123f7c26a83cb1bc4bc
keitao7gawa <keitaogawano@gmail.com>
2025-07-30 15:54:04 +0900

    fix: 入力時にもエラーダイアログが表示されるバクを修正

--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -20,10 +20,10 @@ const inputUserName = ref("")
 // 入室メッセージをクライアントに送信する
 const onEnter = () => {
   // ユーザー名が入力されているかチェック
-  // if (inputUserName.value == "") {
-  //   alert("ユーザー名を入力してください")
-  //   return
-  // }
+  if (inputUserName.value === "") {
+    alert("ユーザー名を入力してください")
+    return
+  }
   // 入室メッセージを送信
 
   // 全体で使用するnameに入力されたユーザー名を格納
@@ -39,7 +39,7 @@ const onEnter = () => {
     <h1 class="text-h3 font-weight-medium">Vue.js Chat サンプル</h1>
     <div class="mt-10">
       <p>ユーザー名</p>
-      <input type="text" class="user-name-text" />
+      <input type="text" class="user-name-text" v-model="inputUserName" />
     </div>
     <button type="button" @click="onEnter" class="button-normal">入室する</button>
   </div>
