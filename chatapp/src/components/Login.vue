db2921989ae452a0658879074bf2bcc9594b7d20
chika-kiichi <chika-1114777@g.ecc.u-tokyo.ac.jp>
2025-07-30 16:04:13 +0900

    login画面のcss

--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -1,51 +1,79 @@
 <script setup>
-import { inject, ref } from "vue"
-import { useRouter } from "vue-router"
-import socketManager from '../socketManager.js'
+import { inject, ref } from "vue";
+import { useRouter } from "vue-router";
+import socketManager from "../socketManager.js";
 
 // #region global state
-const userName = inject("userName")
+const userName = inject("userName");
 // #endregion
 
 // #region local variable
-const router = useRouter()
-const socket = socketManager.getInstance()
+const router = useRouter();
+const socket = socketManager.getInstance();
 // #endregion
 
 // #region reactive variable
-const inputUserName = ref("")
+const inputUserName = ref("");
 // #endregion
 
 // #region browser event handler
 // 入室メッセージをクライアントに送信する
 const onEnter = () => {
-  // ユーザー名が入力されているかチェック
+	// ユーザー名が入力されているかチェック
 
-  // 入室メッセージを送信
+	// 入室メッセージを送信
 
-  // 全体で使用するnameに入力されたユーザー名を格納
+	// 全体で使用するnameに入力されたユーザー名を格納
 
-  // チャット画面へ遷移
-  router.push({ name: "chat" })
-}
+	// チャット画面へ遷移
+	router.push({ name: "chat" });
+};
 // #endregion
 </script>
 
 <template>
-  <div class="mx-auto my-5 px-4">
-    <h1 class="text-h3 font-weight-medium">Vue.js Chat サンプル</h1>
-    <div class="mt-10">
-      <p>ユーザー名</p>
-      <input type="text" class="user-name-text" />
-    </div>
-    <button type="button" @click="onEnter" class="button-normal">入室する</button>
-  </div>
+	<div class="mx-auto my-5 px-4 login-wrapper">
+		<h1 class="title d-flex flex-column align-center">
+			<span>絶対投稿見させる</span><span>チャット</span>
+		</h1>
+		<div class="mt-10 d-flex flex-column justify-center">
+			<input type="text" placeholder="ユーザー名" class="user-name-text" />
+		</div>
+		<button type="button" @click="onEnter" class="button-normal">
+			入室する
+		</button>
+	</div>
 </template>
 
 <style scoped>
+.login-wrapper {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	background-color: #ffffff;
+	padding: 50px 16px;
+	border-radius: 30px;
+}
+@media screen and (min-width: 500px) {
+	.title {
+		font-size: 3rem;
+		font-weight: 400;
+		line-height: 1.05;
+		letter-spacing: normal;
+	}
+}
+@media screen and (max-width: 500px) {
+	.title {
+		font-size: 2rem;
+		font-weight: 400;
+		line-height: 1.05;
+		letter-spacing: normal;
+	}
+}
 .user-name-text {
-  width: 200px;
-  border: 1px solid #888;
-  margin-bottom: 16px;
+	border: 1px solid #888;
+	margin-bottom: 16px;
+	background-color: #ffffff;
+	padding: 2px 2px 2px 6px;
 }
 </style>
